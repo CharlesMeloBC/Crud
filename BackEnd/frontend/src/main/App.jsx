6a0122568074a78cc8e44fd3fd554fddc51c42f3
Charles Melo <charles.melo145@gmail.com>
2022-08-24 23:54:40 -0300

    Organizando componentes

---
+++ BackEnd/frontend/src/main/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Footer from '../components/template/Footer'
+import Nav from '../components/template/Nav'
+import Logo from '../components/template/Logo'
+import Main from '../components/template/Main'
+
+export default props =>
+ <div className="app">
+    <Footer />
+    <Nav />
+    <Logo />
+    <Main />
+ </div>
